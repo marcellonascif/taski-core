14324c5dc315d8bc52910a0bd6c72897c0df9e35
Marcello Nascif <marcellobnf@gmail.com>
2025-05-14 11:49:58 -0300

    adjust path with tsconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
-import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [TaskModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { CreateTaskService } from './create-task.service';
 import { ExtractTaskService } from './extract-task.service';
-import { LlmGeminiService } from 'src/llm-client/llm-gemini.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { LlmGeminiService } from 'src/llm/llm-gemini.service';
+import { PrismaService } from '@db/prisma.service';
 
 @Module({
   controllers: [TaskController],
