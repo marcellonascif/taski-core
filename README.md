73c47c50d662bac5bdf9d273ccc227c8b5190b77
Marcello Nascif <marcellobnf@gmail.com>
2025-05-20 00:52:13 -0300

    update database

--- README.md
+++ README.md
@@ -73,13 +73,8 @@ $ docker run --name taski-core-container --rm --env-file .env -p 3000:3000 taski
 $ docker build --tag taski-core . && docker run --name taski-core-container --rm --env-file .env -p 3000:3000 taski-core
 ```
 
-## Deployment
-
-When you're ready to deploy your NestJS application to production, there are some key steps you can take to ensure it runs as efficiently as possible. Check out the [deployment documentation](https://docs.nestjs.com/deployment) for more information.
-
-If you are looking for a cloud-based platform to deploy your NestJS application, check out [Mau](https://mau.nestjs.com), our official platform for deploying NestJS applications on AWS. Mau makes deployment straightforward and fast, requiring just a few simple steps:
+## To apply changes on Database run
 
 ```bash
-$ npm install -g @nestjs/mau
-$ mau deploy
+npx prisma migrate dev --name init
 ```
---
+++ prisma/migrations/20250520022108_init/migration.sql
@@ -0,0 +1,39 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "plan" TEXT,
+    "scheduledDate" TIMESTAMP(3) NOT NULL,
+    "dueDate" TIMESTAMP(3),
+    "priority" INTEGER NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "category" TEXT NOT NULL,
+    "mentalEnergy" INTEGER NOT NULL,
+    "physicalEnergy" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
