981124c9198aa7a54c27428f20dc2b33155654e0
Marcello Nascif <marcellobnf@gmail.com>
2025-05-26 14:35:41 -0300

    adds verification with hashed password

--- src/application/usecases/sign-in.usecase.ts
+++ src/application/usecases/sign-in.usecase.ts
@@ -2,11 +2,13 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from '@application/repositories/user.repository';
 import { TokenProvider } from '@application/tokens/token.provider';
 import { User } from '@domain/entities/user.entity';
+import { PasswordHasher } from '@application/hashers/password.hasher';
 
 @Injectable()
 export class SignInUseCase {
     constructor(
         private readonly repo: UserRepository,
+        private readonly passwordHasher: PasswordHasher,
         private readonly tokenProvider: TokenProvider,
     ) {}
 
@@ -16,7 +18,11 @@ export class SignInUseCase {
     }): Promise<string> {
         const user: User | null = await this.repo.findByEmail(userData.email);
 
-        if (user?.password !== userData.password) {
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        else if (!await this.passwordHasher.compare(userData.password, user.password)) {
             throw new UnauthorizedException('Invalid credentials');
         }
 
--- src/infrastructure/hashers/bcrypt-password.hasher.ts
+++ src/infrastructure/hashers/bcrypt-password.hasher.ts
@@ -7,14 +7,14 @@ export class BCryptPasswordHasher extends PasswordHasher {
     private readonly saltRounds: number;
     constructor() {
         super();
-        this.saltRounds = 12;
+        this.saltRounds = 10;
     }
 
     async hash(password: string): Promise<string> {
-        return bcrypt.hash(password, this.saltRounds);
+        return await bcrypt.hash(password, this.saltRounds);
     }
 
     async compare(password: string, hashedPassword: string): Promise<boolean> {
-        return bcrypt.compare(password, hashedPassword);
+        return await bcrypt.compare(password, hashedPassword);
     }
 }
